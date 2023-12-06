4d7a23189be9db6fb68177baef3b184948cfcf89
Drew Fowler <drew@drews-air.lan>
2023-12-06 15:18:16 -0500

    Day 03 Done - I should probably clean it up later

--- Day03/Day03_Program.ps1
+++ Day03/Day03_Program.ps1
@@ -35,14 +35,54 @@ function Search-AdjacentCharacter {
     return $foundadjacent
 }
 
+function Get-GearRatioSumByRow {
+    param (
+        [int]$CurrentRowIndex,
+        $SearchMatrix
+    )
+    $gearratiosum = 0
+    # In every case we will search the current row for possible gears and digits
+    $possiblegears = $($SearchMatrix[$CurrentRowIndex] | Select-String -Pattern '\*' -AllMatches).Matches
+    #Write-Host "$($possiblegears.count) possible gears found"
+    $currentrow = $($SearchMatrix[$CurrentRowIndex] | Select-String '\d+' -AllMatches).Matches
+    # If we are on the first row of the search matrix there is no look back
+    if ($CurrentRowIndex -eq 0) {
+        $beforerow = $null
+    } else {
+        $beforerow = $($SearchMatrix[$CurrentRowIndex - 1] | Select-String '\d+' -AllMatches).Matches
+    }
+    # If we are on the last row of the search matrix there is no look ahead
+    if ($CurrentRowIndex -eq ($SearchMatrix.Count -1)) {
+        $afterrow = $null
+    } else {
+        $afterrow = $($SearchMatrix[$CurrentRowIndex + 1] | Select-String '\d+' -AllMatches).Matches
+    }
+    # Since all variables now have 'something' in them, we should be able to appl the same logic
+    # to every row. 
+
+    # Now we loop through the 'possibles'
+    foreach ($p in $possiblegears) {
+        # Initialize a part array to which we will add an item each time we find a match adjacent to 
+        # a possible gear
+        $partlist = @()
+        $partlist += $beforerow | Where-Object -FilterScript {(($_.Index -le ($p.Index + $p.Length)) -and (($_.Index + $_.Length - 1) -ge ($p.Index -1)))} | Select-Object -ExpandProperty Value
+        $partlist += $currentrow | Where-Object -FilterScript {(($_.Index + $_.Length - 1) -eq $p.Index -1) -or ($_.Index -eq ($p.Index + $p.Length))} | Select-Object -ExpandProperty Value
+        $partlist += $afterrow | Where-Object -FilterScript {(($_.Index -le ($p.Index + $p.Length)) -and (($_.Index + $_.Length - 1) -ge ($p.Index -1)))} | Select-Object -ExpandProperty Value 
+        # If the partlist count is exactly 2, multiply them together and add them to gearratiosum
+        if ($partlist.count -eq 2) {$gearratiosum = $gearratiosum + ([int]$partlist[0] * [int]$partlist[1])}
+    }
+    return $gearratiosum
+}
+
 # Read the input data
 $inp = Get-Content ./Day03_input.txt
 # Initialize the total value
 $Total = 0
+$GearTotal = 0
 # Get the maximum index by looking at the count property of $inp and subtracting 1
 $maxindex = $inp.Count - 1
 
-# Loop through rows 2-139 (or whatever the maximum - 1 is)
+# Loop through rows 0-139 (or whatever the maximum - 1 is)
 foreach ($r in $(0..($maxindex))) {
     $partnums = @()
     $nonpartnums = @()
@@ -56,6 +96,8 @@ foreach ($r in $(0..($maxindex))) {
         }
         if ($ispartnum) {$partnums += $num.Value} else {$nonpartnums += $num.Value}
     }
+    $GearTotal = $GearTotal + (Get-GearRatioSumByRow -CurrentRowIndex $r -SearchMatrix $inp)
 }
 
 Write-Host "Part 1 Answer is $Total"
+Write-Host "Part 2 Answer is $GearTotal"
--- Day03/README.md
+++ Day03/README.md
@@ -1,3 +1,6 @@
 Blehhhhh!!!! I had such a time with all the logcal conditions involved in searching around a number in a 2D array. I kept getting the wrong number over and over. Eventually I went through the data and results and found that I didn't have logic that account for a match at the end of a row. When I tried to account for that, I didn't catch the case where the end of the row entry was also a match. I functionalized the search after which it still didn't work. Eventually I found that I had omitted one of the comparison operators in one of my if statements.
 
+The approach I used in part 2 could be cleaned up to apply to part 1 as well. It would make the script better. Maybe I will come back later and fix it up. 
+
 Part 1 Answer is 554003
+Part 2 Answer is 87263515
